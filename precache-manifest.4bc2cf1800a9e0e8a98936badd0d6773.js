46950d8820f26f9156331fbaaf40f4805cf3e2af
Pavlitsa <pzafeiri@gmail.com>
2020-03-01 16:37:18 +0100

    Updates

---
+++ precache-manifest.4bc2cf1800a9e0e8a98936badd0d6773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d729518dd203c168ecc43fd53cb4042b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e3dce1a8f530324c7505",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c8dc7ae1697ce4811a85",
+    "url": "/robofriends/static/css/main.3af854ff.chunk.css"
+  },
+  {
+    "revision": "e3dce1a8f530324c7505",
+    "url": "/robofriends/static/js/2.6bcfb87d.chunk.js"
+  },
+  {
+    "revision": "c8dc7ae1697ce4811a85",
+    "url": "/robofriends/static/js/main.bfd520f1.chunk.js"
+  },
+  {
+    "revision": "efd7d2fdff39d75600cb",
+    "url": "/robofriends/static/js/runtime-main.9e40dea0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
